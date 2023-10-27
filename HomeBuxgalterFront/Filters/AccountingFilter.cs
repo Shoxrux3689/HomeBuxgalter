183ec9592a46ceb99cac4199dc9e017433aa6206
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-10-27 18:18:08 +0500

    a

--- HomeBuxgalterFront/Filters/AccountingFilter.cs
+++ HomeBuxgalterFront/Filters/AccountingFilter.cs
@@ -1,10 +1,21 @@
-﻿namespace HomeBuxgalterFront.Filters;
+﻿using HomeBuxgalterFront.Filters.Enums;
+
+namespace HomeBuxgalterFront.Filters;
 
 public class AccountingFilter
 {
     public long? StartAmount { get; set; }
     public long? EndAmount { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public DateTime StartDate { get; set; } = DateTime.Parse($"{DateTime.Now.Month}/1/{DateTime.Now.Year}");
+    public DateTime EndDate { get; set; } = DateTime.Now;
     public EBy ByWhichTime { get; set; }
+
+
+    public override string ToString()
+    {
+        string urlQuery = $"{StartAmount}={StartAmount}&{EndAmount}={EndAmount}&" +
+            $"{StartDate}={StartDate}&{EndDate}={EndDate}&{ByWhichTime}={ByWhichTime}";
+
+        return urlQuery;
+    }
 }
--- HomeBuxgalterFront/Program.cs
+++ HomeBuxgalterFront/Program.cs
@@ -1,4 +1,5 @@
 using HomeBuxgalterFront;
+using HomeBuxgalterFront.Managers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,6 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7799") });
-
+builder.Services.AddScoped<RequestManager>();
 
 await builder.Build().RunAsync();
