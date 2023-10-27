b1baed2a99607c9113d9e9d50ffa8418327f4246
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-10-27 14:34:14 +0500

    a

---
+++ HomeBuxgalterFront/Models/Outlay.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeBuxgalterFront.Models;
+
+public class Outlay
+{
+    public int Id { get; set; }
+    public long Amount { get; set; }
+    public required DateTime Date { get; set; }
+    public string? Comment { get; set; }
+    public required string Category { get; set; }
+}
