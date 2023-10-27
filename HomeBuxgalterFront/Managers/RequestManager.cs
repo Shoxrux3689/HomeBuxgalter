279c57cf71d6da6ad58fc89902a1c7de6269e876
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-10-27 17:43:19 +0500

    a

---
+++ HomeBuxgalterFront/Managers/RequestManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
+
+
+
+namespace HomeBuxgalterFront.Managers;
+
+public class RequestManager
+{
+    public HttpClient _httpClient;
+
+    public RequestManager(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<T?> Get<T>()
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, "/api/profits");
+
+        var response = await _httpClient.SendAsync(request);
+
+        var model = await response.Content.ReadFromJsonAsync<T?>();
+
+        return model;
+    }
+}
---
+++ HomeBuxgalterFront/Models/ReportModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeBuxgalterFront.Models;
+
+public class ReportModel
+{
+    public DateTime Date { get; set; }
+    public long Balance { get; set; }
+    public long OutlaySummary { get; set; }
+    public long ProfitSummary { get; set; }
+}
