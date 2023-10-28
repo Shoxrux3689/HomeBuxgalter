31d1d3dbcd8d001c8c6bda0d52eff2e2990a285e
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-10-28 19:03:22 +0500

    a

--- HomeBuxgalterFront/Models/ProfitModels/ProfitCategory.cs
+++ HomeBuxgalterFront/Models/ProfitModels/ProfitCategory.cs
@@ -3,5 +3,13 @@
 public class ProfitCategory
 {
     public short Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get 
+        {
+            return "Quyidagilardan tanlang";
+        }
+        set 
+        {
+            Name = value;
+        }
+    }
 }
