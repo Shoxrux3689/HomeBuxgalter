00c6a6004174eb6cc02b33e9c9e7925b2be7dd51
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-10-31 23:02:01 +0500

    a

--- HomeBuxgalterFront/Filters/AccountingFilter.cs
+++ HomeBuxgalterFront/Filters/AccountingFilter.cs
@@ -10,6 +10,7 @@ public class AccountingFilter
 
     public override string ToString()
     {
+        EndDate = DateTime.Parse($"{EndDate.Month}/{EndDate.Day}/{EndDate.Year} 23:59:59");
         var urlQuery = $"StartDate={StartDate.ToShortDateString()}&EndDate={EndDate.ToString()}&ByWhichTime={ByWhichTime}";
         
         return urlQuery;
