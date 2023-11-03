ef2535d7a927b220ac1e13d593947d2ea13f8d12
shoxrux <shoxruxxaydarov3689@gmail.com>
2023-11-03 11:17:01 +0500

    a

--- HomeBuxgalter/Context/AppDbContext.cs
+++ HomeBuxgalter/Context/AppDbContext.cs
@@ -11,4 +11,24 @@ public class AppDbContext : DbContext
     public DbSet<ProfitCategory> ProfitCategories { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProfitCategory>().HasData(new List<ProfitCategory>()
+        {
+            new ProfitCategory() { Name = "Заработная плата"},
+            new ProfitCategory() { Name = "Дохода с сдачи в аренду недвижимости"},
+            new ProfitCategory() { Name = "Иные доходы"},
+        });
+
+        modelBuilder.Entity<OutlayCategory>().HasData(new List<OutlayCategory>()
+        {
+            new OutlayCategory() { Name = "Продукты питания"},
+            new OutlayCategory() { Name = "Транспорт"},
+            new OutlayCategory() { Name = "Мобильная связь"},
+            new OutlayCategory() { Name = "Интернет"},
+            new OutlayCategory() { Name = "Развлечения"},
+            new OutlayCategory() { Name = "Другое"}
+        });
+    }
 }
